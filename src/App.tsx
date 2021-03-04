3957175baa23269076589153cd3f7d202c517cbc
Shea Meyers <shea.meyers@gmail.com>
2021-03-04 18:09:24 +0100

    Begin building app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+const useStyles = makeStyles({
+  root: {
+    width: "90%",
+    margin: 20
+  },
+  addPizza: {
+    width: "90%",
+    margin: 20,
+    display: 'flex'
+  }
+});
+
+
 function App() {
+
+  const classes = useStyles();
+  const [choices, setChoices] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className={classes.root}>
+        <CardContent>
+          <h1>Pizza Area Comparer</h1>
+          <h3>Find out which pizzas give the best deals</h3>
+        </CardContent>
+      </Card>
+      <Card className={classes.addPizza}>
+        <CardContent>
+          <TextField id="diameter-basic" label="Diameter" variant="outlined" />
+          <TextField id="price-basic" label="Price" variant="outlined" />
+          <Button variant="contained" color="primary">
+            Add
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
